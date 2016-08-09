d1671d88921d4ede8e1e422b9b027d377c6e2b66
DummyCo <ashurillia@gmail.com>
2016-08-09 04:45:04 +0300

    Base DrawerActivity add

--- IntranetMobile.Droid/Views/NewsActivity.cs
+++ IntranetMobile.Droid/Views/NewsActivity.cs
@@ -1,53 +1,12 @@
 using Android.App;
-using Android.Support.Design.Widget;
-using Android.Support.V4.Widget;
-using Android.Views;
-using Android.Widget;
 using IntranetMobile.Core.ViewModels;
-using MvvmCross.Droid.Support.V7.AppCompat;
-using Toolbar = Android.Support.V7.Widget.Toolbar;
 
 namespace IntranetMobile.Droid.Views
 {
     [Activity(Label = "NewsActivity", Theme = "@style/BSTheme")]
-    public class NewsActivity : MvxAppCompatActivity<NewsViewModel>
+    public class NewsActivity : DrawerActivity<NewsViewModel>
     {
-        DrawerLayout drawerLayout;
-        NavigationView navigationView;
-
-        protected override void OnViewModelSet()
-        {
-            base.OnViewModelSet();
-
-            SetContentView(Resource.Layout.News);
-
-            var toolbar = FindViewById<Toolbar>(Resource.Id.news_toolbar);
-
-            SetSupportActionBar(toolbar);
-            SupportActionBar.Title = "Binary studio";
-            SupportActionBar.Subtitle = "News";
-            SupportActionBar.SetHomeAsUpIndicator(Resource.Drawable.ic_menu_white_24dp);
-            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
-
-            drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
-            navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
-
-            navigationView.NavigationItemSelected += (sender, e) => {
-                e.MenuItem.SetChecked(true);
-                //react to click here and swap fragments or navigate
-                drawerLayout.CloseDrawers();
-            };
-        }
-
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            switch (item.ItemId)
-            {
-                case Android.Resource.Id.Home:
-                    drawerLayout.OpenDrawer(Android.Support.V4.View.GravityCompat.Start);
-                    return true;
-            }
-            return base.OnOptionsItemSelected(item);
-        }
+        protected override int LayoutResId { get; } = Resource.Layout.News;
+        protected override string Subtitle { get; } = "News";
     }
 }
