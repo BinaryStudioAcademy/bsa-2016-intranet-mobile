38092662e0107bfd6a6763d6880064db27faacba
andriy.dudka <andriy.dudka@gmail.com>
2016-08-10 21:20:57 +0300

    add methods in INewsService

---
+++ IntranetMobile.Core/Models/Dtos/CompNewsDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace IntranetMobile.Core
+{
+	public class CompNewsDto
+	{
+		[JsonProperty("_id")]
+		public string NewsId { get; set; }
+
+		[JsonProperty("author")]
+		public string AuthorId { get; set; }
+		public string Title { get; set; }
+
+		public string Body { get; set; }
+
+		public long Date { get; set; }
+
+		public string Type { get; set;}
+
+		public int _V { get; set;}
+
+		public List<string> Restrict_ids { get; set;}
+
+		public List<string> Access_roles { get; set; }
+
+		public List<string> Likes { get; set; }
+
+		public List<CommentDto> Comments { get; set; }
+	}
+}
+
---
+++ IntranetMobile.Core/Models/Dtos/WeekNewsDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace IntranetMobile.Core
+{
+	public class WeekNewsDto
+	{
+		[JsonProperty("_id")]
+		public string WeekliesId { get; set; }
+
+		public string Title { get; set; }
+
+		[JsonProperty("author")]
+		public string AuthorId { get; set; }
+
+		public long Date { get; set; }
+
+		public List<string> News { get; set; }
+
+		public bool Published { get; set; }
+
+		public int __V { get; set; }
+
+		public List<FullNewsDto> FullNews { get; set; }
+	}
+}
+
