7309aad20f184eb38faf0686d44d4da861914fa7
ilchenkob <ilchenkob@gmail.com>
2016-08-13 00:45:43 +0300

    Added app icon

--- IntranetMobile.Droid/Resources/Resource.Designer.cs
+++ IntranetMobile.Droid/Resources/Resource.Designer.cs
@@ -3661,19 +3661,22 @@ namespace IntranetMobile.Droid
 			public const int ic_home_black_24dp = 2130837593;
 			
 			// aapt resource value: 0x7f02005a
-			public const int ic_menu_white_24dp = 2130837594;
+			public const int ic_launcher = 2130837594;
 			
 			// aapt resource value: 0x7f02005b
-			public const int ic_settings_black_24dp = 2130837595;
+			public const int ic_menu_white_24dp = 2130837595;
 			
 			// aapt resource value: 0x7f02005c
-			public const int Icon = 2130837596;
+			public const int ic_settings_black_24dp = 2130837596;
 			
 			// aapt resource value: 0x7f02005d
-			public const int logo_black_1 = 2130837597;
+			public const int Icon = 2130837597;
 			
 			// aapt resource value: 0x7f02005e
-			public const int notification_template_icon_bg = 2130837598;
+			public const int logo_black_1 = 2130837598;
+			
+			// aapt resource value: 0x7f02005f
+			public const int notification_template_icon_bg = 2130837599;
 			
 			static Drawable()
 			{
--- IntranetMobile.Droid/Views/LoginActivity.cs
+++ IntranetMobile.Droid/Views/LoginActivity.cs
@@ -6,7 +6,7 @@ using FragmentTransaction = Android.Support.V4.App.FragmentTransaction;
 
 namespace IntranetMobile.Droid.Views
 {
-    [Activity(Label = "IntranetMobile.Droid", MainLauncher = true, Icon = "@drawable/icon", Theme = "@style/BSTheme", NoHistory = true)]
+    [Activity(Label = "Intranet Mobile", MainLauncher = true, Icon = "@drawable/ic_launcher", Theme = "@style/BSTheme", NoHistory = true)]
     public class LoginActivity : MvxCachingFragmentCompatActivity<LoginViewModel>
     {
         protected override void OnViewModelSet()
