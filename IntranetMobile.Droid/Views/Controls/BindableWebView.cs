b55533c9790a6197934c2c55d23c7b5dbc29eb8f
DummyCo <ashurillia@gmail.com>
2016-08-17 00:52:03 +0300

    Unpredicted LoadDataWithBaseURL WebView method remove

--- IntranetMobile.Droid/Views/Controls/BindableWebView.cs
+++ IntranetMobile.Droid/Views/Controls/BindableWebView.cs
@@ -27,7 +27,7 @@ namespace IntranetMobile.Droid.Views.Controls
                     "<html><head><style>pre{white-space:pre-wrap;white-space:-moz-pre-wrap;white-space:-pre-wrap;white-space:-o-pre-wrap;word-wrap:break-word;}</style><style>div{max-width:100%;width:auto;height:auto}</style><style>img{display:inline;height:auto;max-width:100%}</style><style>body{font-family:'Roboto'}</style></head><body>" +
                     _text + "</body></html>";
 
-                LoadDataWithBaseURL(null, htmlString, "text/html; charset=utf-8", "utf-8", null);
+                LoadData(htmlString, "text/html; charset=utf-8", "utf-8");
                 UpdatedHtmlContent();
             }
         }
