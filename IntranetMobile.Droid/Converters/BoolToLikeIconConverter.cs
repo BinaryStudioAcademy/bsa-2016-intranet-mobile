cd60bd119be4373ff788b78e187f04aa685cc6cf
ilchenkob <ilchenkob@gmail.com>
2016-08-19 00:55:22 +0300

    Added value converter for the 'like' icon

---
+++ IntranetMobile.Droid/Converters/BoolToLikeIconConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MvvmCross.Platform.Converters;
+
+namespace IntranetMobile.Droid
+{
+    public class BoolToLikeIconConverter : MvxValueConverter<bool, string>
+    {
+        protected override string Convert(bool value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value ? "ic_favorite_white_24dp" : "ic_favorite_border_white_24dp";
+        }
+    }
+}
+
