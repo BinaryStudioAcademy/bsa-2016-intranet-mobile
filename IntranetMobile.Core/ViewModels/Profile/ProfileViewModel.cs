17e80bb3125f9c55b3aec5b22e61ca68ff799753
DummyCo <ashurillia@gmail.com>
2016-08-21 18:55:44 +0300

    Profile project files add

--- IntranetMobile.Core/ViewModels/Profile/ProfileViewModel.cs
+++ IntranetMobile.Core/ViewModels/Profile/ProfileViewModel.cs
@@ -1,11 +1,7 @@
-﻿using IntranetMobile.Core.Models;
-
-namespace IntranetMobile.Core.ViewModels.Profile
+﻿namespace IntranetMobile.Core.ViewModels.Profile
 {
     public class ProfileViewModel : BaseViewModel
     {
-        private User _dataModel;
-
         public ProfileViewModel()
         {
             Title = "Profile";
