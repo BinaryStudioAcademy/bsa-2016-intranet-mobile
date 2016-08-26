1a6e0ca1739879eee06f235ebf07b2c2dd34a458
Vitaly Ilchenko <ilchenkob@gmail.com>
2016-08-26 17:36:20 +0300

    Fixed comment input field

--- IntranetMobile.Droid/Views/Activities/CommentsActivity.cs
+++ IntranetMobile.Droid/Views/Activities/CommentsActivity.cs
@@ -3,7 +3,7 @@ using IntranetMobile.Core.ViewModels.News;
 
 namespace IntranetMobile.Droid.Views.Activities
 {
-    [Activity(Label = "CommentsActivity", Theme = "@style/BSTheme")]
+    [Activity(Label = "CommentsActivity", Theme = "@style/BSTheme", WindowSoftInputMode = Android.Views.SoftInput.AdjustPan)]
     public class CommentsActivity : BaseToolbarActivity<CommentsViewModel>
     {
         public override int ActivityLayout { get; } = Resource.Layout.activity_comments;
