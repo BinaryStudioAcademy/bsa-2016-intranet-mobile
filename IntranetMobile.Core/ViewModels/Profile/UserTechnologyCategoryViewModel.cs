20cf92761082415347375a5a6e11d3be30477fce
DummyCo <ashurillia@gmail.com>
2016-08-30 23:19:30 +0300

    Profile technology categories add

---
+++ IntranetMobile.Core/ViewModels/Profile/UserTechnologyCategoryViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+
+namespace IntranetMobile.Core.ViewModels.Profile
+{
+    public class UserTechnologyCategoryViewModel : BaseViewModel
+    {
+        private string _name;
+
+        public ObservableCollection<UserTechnologyViewModel> UserTechnologyViewModels { get; } =
+            new ObservableCollection<UserTechnologyViewModel>();
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                RaisePropertyChanged(() => Name);
+            }
+        }
+    }
+}
