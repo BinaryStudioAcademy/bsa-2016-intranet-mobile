db6fc27913dfdea5ca5b844569f3cf0fcb42cc8a
DummyCo <ashurillia@gmail.com>
2016-09-01 23:34:47 +0300

    TicketOfferViewModel improvement

--- IntranetMobile.Core/ViewModels/Reviewer/TicketOfferViewModel.cs
+++ IntranetMobile.Core/ViewModels/Reviewer/TicketOfferViewModel.cs
@@ -1,15 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using IntranetMobile.Core.Models;
+using IntranetMobile.Core.Services;
 
 namespace IntranetMobile.Core.ViewModels.Reviewer
 {
     public class TicketOfferViewModel : BaseViewModel
     {
-        public string Name { get; } = "Offer name";
+        private string _position = string.Empty;
+        private User _user;
+        private string _userId;
 
-        public string Position { get; } = "Offer position";
+        public TicketOfferViewModel(string userId)
+        {
+            Init(userId);
+        }
+
+        public string UserId
+        {
+            get { return _userId; }
+            set
+            {
+                _userId = value;
+                GetUserData();
+            }
+        }
+
+        public User User
+        {
+            get { return _user; }
+            set
+            {
+                _user = value;
+                RaisePropertyChanged(() => Name);
+            }
+        }
+
+        public string Name => User?.FullName;
+
+        public string Position
+        {
+            get { return _position; }
+            set
+            {
+                _position = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private void GetUserData()
+        {
+            Task.Run(async () =>
+            {
+                User = await ServiceBus.UserService.GetUserByServerId(UserId);
+                var userInfo = await ServiceBus.UserService.GetUserInfoById(User.UserId);
+                Position = userInfo?.Department ?? "?";
+            });
+        }
+
+        public void Init(string userId)
+        {
+            UserId = userId;
+        }
     }
 }
