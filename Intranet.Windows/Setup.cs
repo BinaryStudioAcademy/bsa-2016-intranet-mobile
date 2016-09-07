d570b18d9d9e1949667642243e1c19ecc82ea927
ilchenkob <ilchenkob@gmail.com>
2016-09-07 23:46:51 +0300

    Windows. Code refactoring

--- Intranet.Windows/Setup.cs
+++ Intranet.Windows/Setup.cs
@@ -1,20 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls;
 using Intranet.WindowsUWP.Services;
 using IntranetMobile.Core;
 using IntranetMobile.Core.Interfaces;
 using IntranetMobile.Core.Services;
 using IntranetMobile.Core.ViewModels.Login;
-using MvvmCross.Core.Platform;
 using MvvmCross.Core.ViewModels;
-using MvvmCross.Core.Views;
 using MvvmCross.Platform;
-using MvvmCross.Platform.Platform;
-using MvvmCross.Platform.Plugins;
 using MvvmCross.Plugins.Sqlite;
 using MvvmCross.WindowsUWP.Platform;
 using MvvmCross.WindowsUWP.Views;
@@ -33,8 +25,6 @@ namespace Intranet.WindowsUWP
 
         protected override IMvxApplication CreateApp()
         {
-            //Initialize any dependencies here
-
             Mvx.RegisterSingleton<ILogger>(new WindowsLogger());
             Mvx.RegisterSingleton<IAlertService>(new AlertService());
             Mvx.RegisterSingleton<ISettingsService>(new SettingsService());
