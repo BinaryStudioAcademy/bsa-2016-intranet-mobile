d1c664e055a9d454fd9001d19b78c0edd02a1946
DummyCo <ashurillia@gmail.com>
2016-09-22 18:55:14 +0300

    UWP AlertService update

--- Intranet.Windows/Services/AlertService.cs
+++ Intranet.Windows/Services/AlertService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using Windows.UI.Notifications;
 using Windows.UI.Popups;
 using IntranetMobile.Core.Interfaces;
+using Microsoft.Toolkit.Uwp.Notifications;
 
 namespace Intranet.WindowsUWP.Services
 {
@@ -8,23 +10,50 @@ namespace Intranet.WindowsUWP.Services
     {
         public void ShowPopupMessage(string text)
         {
-            //throw new NotImplementedException();
+            var visual = new ToastVisual
+            {
+                BindingGeneric = new ToastBindingGeneric
+                {
+                    Children =
+                    {
+                        new AdaptiveText
+                        {
+                            Text = "Binary studio"
+                        },
+                        new AdaptiveText
+                        {
+                            Text = text
+                        }
+                    }
+                }
+            };
+
+            var toastContent = new ToastContent
+            {
+                Visual = visual
+            };
+
+            var toast = new ToastNotification(toastContent.GetXml());
+
+            ToastNotificationManager.CreateToastNotifier()
+                .Show(toast);
         }
 
         public async void ShowMessageBox(string title, string text)
         {
-            var dialog = new MessageDialog(text) { Title = title };
+            var dialog = new MessageDialog(text) {Title = title};
             await dialog.ShowAsync();
         }
 
-        public async void ShowDialogBox(string title, string text, string okButtonCaption, string cancelButtonCaption, Action okButtonAction)
+        public async void ShowDialogBox(string title, string text, string okButtonCaption, string cancelButtonCaption,
+            Action okButtonAction)
         {
-            var dialog = new MessageDialog(text) { Title = title };
-            dialog.Commands.Add(new UICommand { Label = okButtonCaption, Id = 0 });
-            dialog.Commands.Add(new UICommand { Label = cancelButtonCaption, Id = 1 });
+            var dialog = new MessageDialog(text) {Title = title};
+            dialog.Commands.Add(new UICommand {Label = okButtonCaption, Id = 0});
+            dialog.Commands.Add(new UICommand {Label = cancelButtonCaption, Id = 1});
             var res = await dialog.ShowAsync();
 
-            if ((int)res.Id == 0)
+            if ((int) res.Id == 0)
             {
                 okButtonAction?.Invoke();
             }
